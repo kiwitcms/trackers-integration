b6801e5ef67d23df051f64e7c53c16f9edb2360c
Alexander Todorov <atodorov@otb.bg>
2022-08-18 22:37:20 +0300

    Multiple adjustments & renames

---
+++ tcms_settings_dir/openproject.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2022 Alexander Todorov <atodorov@MrSenko.com>
+
+# Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+# pylint: disable=undefined-variable
+
+if "tcms_openproject.tracker.OpenProject" not in EXTERNAL_BUG_TRACKERS:  # noqa: F821
+    EXTERNAL_BUG_TRACKERS.append("tcms_openproject.tracker.OpenProject")  # noqa: F821
