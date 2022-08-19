8324cf94a44e38c09adee91daae35765237350e0
Alexander Todorov <atodorov@otb.bg>
2022-08-19 22:27:09 +0300

    Add boilerplate code without implementation - raises exceptions

---
+++ trackers_integration/issuetracker/__init__.py
@@ -0,0 +1,7 @@
+# pylint: disable=unused-import
+
+# Copyright (c) 2022 Alexander Todorov <atodorov@MrSenko.com>
+#
+# Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+
+from .openproject import OpenProject  # noqa
