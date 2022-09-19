ef07c9a30429245a492d2803351d1c2bdfb495c1
Alexander Todorov <atodorov@otb.bg>
2022-09-19 23:49:00 +0300

    Update doc-strings

--- trackers_integration/issuetracker/__init__.py
+++ trackers_integration/issuetracker/__init__.py
@@ -3,5 +3,10 @@
 # Copyright (c) 2022 Alexander Todorov <atodorov@MrSenko.com>
 #
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
+"""
+    Extra Issue Tracker integration between Kiwi TCMS and various
+    Issue Trackers.
 
+    .. versionadded:: 11.6-Enterprise
+"""
 from .openproject import OpenProject  # noqa
