fec0d5b92faaf0203a3f50b114d26ce98c44085c
Alexander Todorov <atodorov@otb.bg>
2022-10-06 13:21:24 +0300

    Build a default MantisBT container for testing

---
+++ tests/mantis/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'mantis_db';
+$g_db_type                = 'pgsql';
+$g_database_name          = 'mantisbt';
+$g_db_username            = 'mantis-root';
+$g_db_password            = 'mantis-password';
+
+$g_default_timezone       = 'Europe/Sofia';
+
+$g_crypto_master_salt     = 'GexJ4FBnzM0tYEV9kdrOD7IS5xgKbK6CX9AbD+ShGPc=';
