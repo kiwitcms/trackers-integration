245877453be092376952247a4864c2488ff99d6f
Alexander Todorov <atodorov@otb.bg>
2023-05-10 16:16:21 +0300

    Reorganize how settings are automatically configured
    to avoid excessively typing module names multiple times

--- tcms_settings_dir/trackers_integration.py
+++ tcms_settings_dir/trackers_integration.py
@@ -3,18 +3,9 @@
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 # pylint: disable=undefined-variable
 
-if (
-    "trackers_integration.issuetracker.OpenProject"
-    not in EXTERNAL_BUG_TRACKERS  # noqa: F821
+for module_name in (
+    "trackers_integration.issuetracker.OpenProject",
+    "trackers_integration.issuetracker.Mantis",
 ):
-    EXTERNAL_BUG_TRACKERS.append(  # noqa: F821
-        "trackers_integration.issuetracker.OpenProject"
-    )
-
-if (
-    "trackers_integration.issuetracker.Mantis"
-    not in EXTERNAL_BUG_TRACKERS  # noqa: F821
-):
-    EXTERNAL_BUG_TRACKERS.append(  # noqa: F821
-        "trackers_integration.issuetracker.Mantis"
-    )
+    if module_name not in EXTERNAL_BUG_TRACKERS:  # noqa: F821
+        EXTERNAL_BUG_TRACKERS.append(module_name)  # noqa: F821
