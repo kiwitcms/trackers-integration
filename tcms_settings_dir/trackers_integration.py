00fc9edbfde73c443556fe78cf40907d8a14fc43
Alexander Todorov <atodorov@otb.bg>
2023-10-24 11:57:02 +0300

    Override IssueTracker RPC credentials with personal ApiTokens

--- tcms_settings_dir/trackers_integration.py
+++ tcms_settings_dir/trackers_integration.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Alexander Todorov <atodorov@MrSenko.com>
+# Copyright (c) 2022-2023 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 # pylint: disable=undefined-variable
@@ -9,3 +9,8 @@ for module_name in (
 ):
     if module_name not in EXTERNAL_BUG_TRACKERS:  # noqa: F821
         EXTERNAL_BUG_TRACKERS.append(module_name)  # noqa: F821
+
+
+# allow RPC communications with e.g. JIRA to appear as if they are coming
+# from the currently logged-in user if they had defined an override API token
+EXTERNAL_ISSUE_RPC_CREDENTIALS = "trackers_integration.auth.personal_api_token"
