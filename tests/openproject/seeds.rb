39ec80a4a12c5206be8509fc8a0827df76b33824
Alexander Todorov <atodorov@otb.bg>
2023-11-08 16:30:25 +0200

    Add a new user to OpenProject DB

--- tests/openproject/seeds.rb
+++ tests/openproject/seeds.rb
@@ -1,3 +1,4 @@
+Seeder.log_to_stdout!
 RootSeeder.new.seed!
 
 User.where(login: 'admin', force_password_change: true).update_all(force_password_change: false)
@@ -5,5 +6,22 @@ User.where(login: 'admin', force_password_change: true).update_all(force_passwor
 admin = User.find_by(login: 'admin')
 hashed_value = Token::API.hash_function('26210315639327b10b56b7ef5d2f47f843c0ced3bafd1540fd4ecb30a06fa80f')
 Token::API.create :user_id => admin.id, :value => hashed_value
+
+bot = User.create!(
+    :login => 'kiwitcms-bot',
+    :firstname => 'Testing',
+    :lastname => 'Bot',
+    :mail => 'bot@example.com',
+    :password => 'Hello-World',
+)
+hashed_value = Token::API.hash_function('c48c60020d8f61e612241889eff79e610410f1322811d8c20df25a71f3619e25')
+Token::API.create :user_id => bot.id, :value => hashed_value
+
+demo_project = Project.find_by(name: 'Demo project')
+role = Role.find_by(name: 'Member')
+
+m = bot.memberships.build(project: demo_project)
+m.roles.append(role)
+bot.memberships.append(m)
 
 Project.where(name: 'Demo project').update_all(public: false)
