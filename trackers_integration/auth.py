4fa0a5e247870a57470c44a32fa524385a430c78
Alexander Todorov <atodorov@otb.bg>
2023-11-14 16:44:11 +0200

    Use Django's cache for private API tokens
    to minimize the number of DB queries

--- trackers_integration/auth.py
+++ trackers_integration/auth.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+
 from trackers_integration.models import ApiToken
 
 
@@ -6,9 +8,12 @@ def personal_api_token(issue_tracker):
     if not issue_tracker.request:
         return None
 
-    token = ApiToken.objects.filter(
-        owner=issue_tracker.request.user, base_url=issue_tracker.bug_system.base_url
-    ).first()
+    token = cache.get(
+        f"api-token-of-{issue_tracker.request.user.pk}-for-{issue_tracker.bug_system.pk}",
+        ApiToken.objects.filter(
+            owner=issue_tracker.request.user, base_url=issue_tracker.bug_system.base_url
+        ).first()
+    )
     if token:
         return (token.api_username, token.api_password)
 
