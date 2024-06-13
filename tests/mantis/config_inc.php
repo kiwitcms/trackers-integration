378a057d8f2bd426703b240fdeb08e9e18b4bcba
Alexander Todorov <atodorov@otb.bg>
2024-06-13 13:43:32 +0300

    Relicense this package under AGPLv3+
    this is GNU Affero General Public License v3 or later!
    
    There are 6 total contributors in this code base, listed by number of
    contributions:
    
    - atodorov                (107 commits)
    - pre-commit-ci[bot]      (23 commits)
    - dependabot[bot] 	  (4 commits)
    - suntorytimed            (2 commits) <--- CONTRIBUTIONS REMOVED
    - pyup-bot                (1 commit)
    - cmbahadir               (1 commit)  <--- GAVE ME EXPLICIT PERMISSION
    
    This is myself + several automated services which I have enabled on this
    repository + explicit permission from @cmbahadir to relicense under AGPLv3+!
    
    At the time of this commit I don't have relicense permission from
    @suntorytimed so his contributions have been removed!

--- tests/mantis/config_inc.php
+++ tests/mantis/config_inc.php
@@ -1,4 +1,10 @@
 <?php
+
+// Copyright (c) 2022 Alexander Todorov <atodorov@otb.bg>
+//
+// Licensed under GNU Affero General Public License v3 or later (AGPLv3+)
+// https://www.gnu.org/licenses/agpl-3.0.html
+
 $g_hostname               = 'mantis_db';
 $g_db_type                = 'pgsql';
 $g_database_name          = 'mantisbt';
